6146a0567db053f5854a1d215373eba591ad810c
victorpallas1 <victorpallas@seguridadea1.com>
2025-04-21 14:35:27 +0000

    ultimos cambios

--- src/datastructures.py
+++ src/datastructures.py
@@ -9,54 +9,36 @@ class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = [
-            {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jane",
-                "last_name": last_name,
-                "age": 35,
-                "lucky_numbers": [10, 14, 3]
-            },
-            {
-                "id": self._generate_id(),
-                "first_name": "Jimmy",
-                "last_name": last_name,
-                "age": 5,
-                "lucky_numbers": [1]
-            }
-        ]
+        self._members = []
 
-    # This method generates a unique incremental ID
+    # This method generates a unique 'id' when adding members into the list (you shouldn't touch this function)
     def _generate_id(self):
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
         member["last_name"] = self.last_name
         member["id"] = self._generate_id()
-        member["lukcky_numbers"] = list(member.get("lucky_numbers", set()))
+        member["lucky_numbers"] = list(member.get("lucky_numbers", set()))
         self._members.append(member)
+
         return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.pop(position)
+                
+                return None
+            
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for member in self._members:
+            if member["id"] == int(id):
+                return member
+            
+        return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
